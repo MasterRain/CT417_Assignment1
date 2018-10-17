bb4f864f258deaadf8baa4e3153ce70d98326f05
Raymond Conlin <rayconlin@gmail.com>
2018-10-17 15:28:12 +0100

    Added Test runner/ still have to input array data

---
+++ src/test/java/TestRunner.java
@@ -0,0 +1,9 @@
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({CourseTest.class,ModuleTest.class, StudentTest.class})
+public class TestRunner {
+
+}
