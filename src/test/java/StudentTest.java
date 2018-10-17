6444c385a029ca20958bbc1f635104d04c180720
Raymond Conlin <rayconlin@gmail.com>
2018-10-17 14:38:24 +0100

    Added student tests

--- src/test/java/StudentTest.java
+++ src/test/java/StudentTest.java
@@ -21,71 +21,108 @@ public class StudentTest {
     final private ArrayList<Course> changeCourses= new ArrayList<Course>();
     final private ArrayList<Module> startModules= new ArrayList<Module>();
     final private ArrayList<Module> changeModules= new ArrayList<Module>();
-
+    private Student test;
 
     @Before
-    public void setUp() throws Exception {
+    public void setUp() {
 
+        test = new Student(startName, startAge, startDoB, startID, startCourses, startModules);
 
     }
 
     @After
-    public void tearDown() throws Exception {
+    public void tearDown() {
     }
 
     @Test
     public void getName() {
+
+        assertEquals(startName,test.getName());
+
     }
 
     @Test
     public void setName() {
+        test.setName(changeName);
+        assertEquals(changeName, test.getName());
     }
 
     @Test
     public void getAge() {
+
+        assertEquals(startAge, test.getAge());
     }
 
     @Test
     public void setAge() {
+
+        test.setAge(changeAge);
+        assertEquals(changeAge, test.getAge());
+
     }
 
     @Test
     public void getDoB() {
+
+        assertEquals(startDoB, test.getDoB());
+
     }
 
     @Test
     public void setDoB() {
+
+        test.setDoB(changeDoB);
+        assertEquals(changeDoB, test.getDoB());
+
     }
 
     @Test
     public void getID() {
+
+        assertEquals(startID, test.getID());
+
     }
 
     @Test
     public void setID() {
+
+        test.setID(changeID);
+        assertEquals(changeID, test.getID());
+
     }
 
     @Test
     public void getUsername() {
-    }
+        assertEquals(""+startName+startAge, test.getUsername());
 
-    @Test
-    public void setUsername() {
+        test.setName(changeName);
+        assertEquals(""+changeName+startAge, test.getUsername());
+
+        test.setAge(changeAge);
+        assertEquals(""+changeName+changeAge, test.getUsername());
     }
 
     @Test
     public void getCourses() {
+        assertEquals(startCourses, test.getCourses());
     }
 
     @Test
     public void setCourses() {
+        test.setCourses(changeCourses);
+        assertEquals(changeCourses, test.getCourses());
     }
 
     @Test
     public void getModules() {
+        assertEquals(startModules, test.getModules());
     }
 
     @Test
     public void setModules() {
+
+        test.setModules(changeModules);
+        assertEquals(changeModules, test.getModules());
+
     }
 }
