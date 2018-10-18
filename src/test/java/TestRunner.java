302da0935d313baba4b25c83bdecfd40f590f810
Raymond Conlin <rayconlin@gmail.com>
2018-10-18 16:48:31 +0100

    removed unesecary file and added mvn test

--- src/test/java/TestRunner.java
+++
@@ -1,9 +0,0 @@
-
-import org.junit.runner.RunWith;
-import org.junit.runners.Suite;
-
-@RunWith(Suite.class)
-@Suite.SuiteClasses({CourseTest.class,ModuleTest.class, StudentTest.class})
-public class TestRunner {
-
-}
